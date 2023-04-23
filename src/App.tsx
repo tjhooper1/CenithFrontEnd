0c704de7a82399e2e9b2b50039ebb11d8d967002
tjhooper1 <tomh386@gmail.com>
2023-04-22 23:16:23 -0400

    Changed control button colors

--- src/App.tsx
+++ src/App.tsx
@@ -63,13 +63,19 @@ function App() {
       return (
         <>
           <div className="UIContainer">
+          <div className="UIContainer__legend">
+              <h3>Blank: <br/> health - 0 <br/> move - 1</h3>
+              <h3>Mud: <br/> health - 10 <br/> moves - 0</h3>
+              <h3>Lava: <br/> health - 50 <br/> moves - 10</h3>
+              <h3>Speeder: <br/> health - 5 <br/> moves - 0</h3>
+            </div>
             <h1>Health: {remainingHealth}</h1>
             <h1>Remaining Moves: {remainingMoves}</h1>
             <h2>New Game:</h2>
             <div className="newGame">
-              <button onClick={() => resetGame(5)}>Easy</button>
-              <button onClick={() => resetGame(10)}>Medium</button>
-              <button onClick={() => resetGame(20)}>Hard</button>
+              <button className="btnGreen" onClick={() => resetGame(5)}>Easy</button>
+              <button className="btnOrange" onClick={() => resetGame(10)}>Medium</button>
+              <button className="btnRed" onClick={() => resetGame(20)}>Hard</button>
             </div>
           </div>
           <GridBoard grid={grid} PlayerPos={playerPos} />
@@ -81,9 +87,9 @@ function App() {
           <h1>You Won!</h1>
           <h2>New Game:</h2>
           <div className="newGame__lostScreen">
-            <button onClick={() => resetGame(5)}>Easy</button>
-            <button onClick={() => resetGame(10)}>Medium</button>
-            <button onClick={() => resetGame(20)}>Hard</button>
+            <button className="btnGreen" onClick={() => resetGame(5)}>Easy</button>
+            <button className="btnOrange" onClick={() => resetGame(10)}>Medium</button>
+            <button className="btnRed" onClick={() => resetGame(20)}>Hard</button>
           </div>
         </>
       );
@@ -93,9 +99,9 @@ function App() {
           <h1>You Lost!</h1>
           <h2>New Game:</h2>
           <div className="newGame__lostScreen">
-            <button onClick={() => resetGame(5)}>Easy</button>
-            <button onClick={() => resetGame(10)}>Medium</button>
-            <button onClick={() => resetGame(20)}>Hard</button>
+            <button className="btnGreen" onClick={() => resetGame(5)}>Easy</button>
+            <button className="btnOrange" onClick={() => resetGame(10)}>Medium</button>
+            <button className="btnRed" onClick={() => resetGame(20)}>Hard</button>
           </div>
         </>
       );
