78deff4f6b1796d2d5665cbbeb2b51c5b443452c
tjhooper1 <tomh386@gmail.com>
2023-04-23 17:15:33 -0400

    removing unused imports

--- src/tests/player.test.ts
+++ src/tests/player.test.ts
@@ -2,7 +2,6 @@ import { describe, it, beforeEach, expect } from "vitest";
 import { renderHook, act } from "@testing-library/react-hooks";
 import { usePlayer } from "../hooks/usePlayer";
 import { GridCell } from "../types/Grids";
-import { PlayerPosition } from "../types/Player";
 
 describe("usePlayer", () => {
   let grid: GridCell[][];
--- src/types/Player.ts
+++ src/types/Player.ts
@@ -2,11 +2,3 @@ export type PlayerPosition = {
     x: number;
     y: number;
 };
-
-export type Player = {
-    position: PlayerPosition;
-    health: number;
-    moves: number;
-}
-
-export type Direction = 'up' | 'down' | 'left' | 'right';
